57d1478f05b642e0963fcd2afc898a2ef29e710b
RMuntoni <calnon95@gmail.com>
2020-12-02 17:31:19 +0100

    Add project files.

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// bookshelf-app/src/index.tsx
+
+// Import deps
+import React from 'react'
+import { render } from 'react-dom'
+
+// Import components
+import { Bookshelf } from './components/main'
+
+// Find div container
+const rootElement = document.getElementById('root')
+
+// Render Bookshelf component in the DOM
+render(<Bookshelf />, rootElement)
